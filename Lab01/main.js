bde98816aa441f0ebee71d43644a4ca46ac5d59e
A-star-ayush <myselfthebest@yahoo.com>
2017-01-28 02:28:12 +0530

    v1.2

--- Lab01/main.js
+++ Lab01/main.js
@@ -5,16 +5,16 @@ window.onload = function() {
 	var canvas = document.getElementById("canvas");
 	ct = canvas.getContext("2d");
 	var width = canvas.width = window.innerWidth;   
-	var height = canvas.height = window.innerHeight;
+	var height = canvas.height = window.innerHeight;  // have the canvas fill the entire space
 
 	var sub = document.getElementById("input");
-	sub.onsubmit = SierpinskiTriangle;
+	sub.onsubmit = SierpinskiTriangle; 	// registering event handler for form submission
 }
 
 function SierpinskiTriangle(e){
 	e.preventDefault();
 	var input = document.getElementById("input");
-	var p1 = input.p1.value.split(",");
+	var p1 = input.p1.value.split(",");  // x,y gets split into x and y
 	var p2 = input.p2.value.split(",");
 	var p3 = input.p3.value.split(",");
 	var d = input.depth.value;
@@ -22,7 +22,7 @@ function SierpinskiTriangle(e){
 	ct.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	
 	for(var i=0;i<=1;++i){
-		p1[i] = parseInt(p1[i], 10);
+		p1[i] = parseInt(p1[i], 10);  // to convert user input from string to integer
 		p2[i] = parseInt(p2[i], 10);
 		p3[i] = parseInt(p3[i], 10);
 	}
@@ -34,7 +34,7 @@ function drawTriangle(ct, p1, p2, p3, depth, maxDepth){
 	if(depth == maxDepth) return;
 	
 	ct.beginPath();
-	ct.moveTo(p1[0], p1[1]);
+	ct.moveTo(p1[0], p1[1]);  // index [0] is the x co-ordinate and [1] is the y co-ordinate
 	ct.lineTo(p2[0], p2[1]);
 	ct.lineTo(p3[0], p3[1]);
 	ct.lineTo(p1[0], p1[1]);
@@ -44,7 +44,7 @@ function drawTriangle(ct, p1, p2, p3, depth, maxDepth){
 	var q2 = [];
 	var q3 = [];
 
-	q1[0] = (p1[0] + p2[0])>>1;
+	q1[0] = (p1[0] + p2[0])>>1;  // >>1 is equivalent to divide by 2 but is much faster and efficient
 	q1[1] = (p1[1] + p2[1])>>1;
 	q2[0] = (p2[0] + p3[0])>>1;
 	q2[1] = (p2[1] + p3[1])>>1;
@@ -56,7 +56,7 @@ function drawTriangle(ct, p1, p2, p3, depth, maxDepth){
 	ct.lineTo(q2[0], q2[1]);
 	ct.lineTo(q3[0], q3[1]);
 	ct.lineTo(q1[0], q1[1]);
-	ct.fill();
+	ct.fill();  // Note the style is fill and not stroke
 
 	drawTriangle(ct, p1, q1, q3, depth+1, maxDepth);
 	drawTriangle(ct, q1, p2, q2, depth+1, maxDepth);
@@ -64,8 +64,7 @@ function drawTriangle(ct, p1, p2, p3, depth, maxDepth){
 
 }
 
-
-	/*
+/*  Sample values
 	var p1 = { x:500, y:500 };
 	var p2 = { x:900, y:100 };
 	var p3 = { x:1300, y:500 };
